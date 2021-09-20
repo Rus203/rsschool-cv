8e0f72ec087a004be8b74e7b4190a046da885574
Ruslan Kharkevich <MyWorkMail3103@gmail.com>
2021-09-20 22:25:40 +0300

    refactor: improve the slider

--- main.js
+++ main.js
@@ -4,13 +4,19 @@ const block_of_burger = document.querySelector(".block_of_burger")
 const showMenu = document.querySelector(".show_menu")
 const cross = document.querySelector(".cross");
 
+
 block_of_burger.addEventListener("click", () => useMenu());
 cross.addEventListener("click", () => useMenu())
+showMenu.addEventListener("click", () => useMenu())
 
 function useMenu() {
   block_of_burger.classList.toggle('experiment');
   showMenu.classList.toggle('experiment2');
   console.log("Script was working!")
+}
+
+function returnBack() {
+  showMenu.classList.toggle('experiment2');
 }
 
 console.log("Script is work");
